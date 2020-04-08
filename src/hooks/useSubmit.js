94defa7402d85c36cb7a6bfc0a6cbc62129bb12b
Charles Howard <frodare@gmail.com>
2020-04-08 16:55:12 -0400

    returning sumbit related meta from useSubmit

--- src/hooks/useSubmit.js
+++ src/hooks/useSubmit.js
@@ -1,6 +1,20 @@
 import useForm from './useForm'
 
+const shouldUpdate = ({ previous, current }) => {
+  if (previous.meta === current.meta) return false
+
+  return previous.meta.submitCount !== current.meta.submitCount ||
+    previous.meta.submitted !== current.meta.submitted ||
+    previous.meta.submitting !== current.meta.submitting
+}
+
 export default () => {
-  const { submit, onSubmit } = useForm({ shouldUpdate: () => false })
-  return { submit, onSubmit }
+  const { submit, onSubmit, meta } = useForm({ shouldUpdate })
+  return {
+    submit,
+    onSubmit,
+    submitted: meta.submitted,
+    submitCount: meta.submitCount,
+    submitting: meta.submitting
+  }
 }
