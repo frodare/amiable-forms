bf999296f67c8ac92530fceeb79572a19c2a3098
Charles Howard <frodare@gmail.com>
2021-09-09 22:15:00 -0400

    added initial values back

--- src/components/AmiableForm.jsx
+++ src/components/AmiableForm.jsx
@@ -1,21 +1,29 @@
-import React, { useRef, createContext, useMemo } from 'react'
+import React, { useRef, createContext, useMemo, useEffect } from 'react'
 import notifier from '../util/notifier'
-// import useFormActions from '../hooks/useFormActions'
 import actionDispatchers from '../state/actionDispatchers'
 import useFormStore from '../state/useFormStore'
 import buildSubmitHandlers from '../util/buildSubmitHandlers'
 
 export const formContext = createContext({})
 
+const useSetInitialValues = (initialValues, setValues) => {
+  useEffect(() => {
+    if (initialValues) {
+      setValues(initialValues)
+    }
+  }, [initialValues, setValues])
+}
+
 const AmiableForm = props => {
   const { validate, transform, initialValues } = props
   const formRef = useRef()
 
   const [notifyStateUpdate, addUpdateHandler, removeUpdateHandler] = useMemo(notifier, [])
-  const [stateRef, dispatch] = useFormStore({ initialValues, transform, validate, notifyStateUpdate })
-  // const actions = useMemo(() => useFormActions(dispatch, formRef), [dispatch, formRef])
+  const [stateRef, dispatch] = useFormStore({ transform, validate, notifyStateUpdate })
   const actions = useMemo(() => actionDispatchers(formRef, dispatch), [formRef, dispatch])
   const { submit, onSubmit } = buildSubmitHandlers({ stateRef, actions, props })
+
+  useSetInitialValues(initialValues, actions.setValues)
 
   formRef.current = {
     ...actions,
--- src/state/useFormStore.js
+++ src/state/useFormStore.js
@@ -1,16 +1,9 @@
-import { useRef, useEffect, useCallback } from 'react'
+import { useRef, useCallback } from 'react'
 import reducerCreator, { initialState } from './reducer'
-import * as actionTypes from './actions'
 
-export default ({ initialValues, transform, validate, notifyStateUpdate }) => {
-  console.log('render useFormStore')
+export default ({ transform, validate, notifyStateUpdate }) => {
   const stateRef = useRef(initialState)
-
   const reducer = useCallback(reducerCreator(transform, validate), [transform, validate])
-
-  // const dispatch = (...args) => console.log('dispatch blocked', args)
-
-  // adding in the reducer causes a infinite loop
 
   const dispatch = useCallback(action => {
     const previous = stateRef.current
@@ -18,13 +11,6 @@ export default ({ initialValues, transform, validate, notifyStateUpdate }) => {
     stateRef.current = current
     notifyStateUpdate({ previous, current })
   }, [notifyStateUpdate, stateRef])
-
-  // useEffect(() => {
-  //   if (initialValues) {
-  //     // FIXME replace with action dispatcher
-  //     dispatch({ type: actionTypes.SET_VALUES, values: initialValues, options: { merge: false } })
-  //   }
-  // }, [initialValues, dispatch])
 
   return [stateRef, dispatch]
 }
