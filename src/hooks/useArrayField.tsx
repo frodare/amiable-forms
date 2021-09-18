0d51d69d620f95e72ef72a7a7feacc31b5a5a6c4
Charles Howard <frodare@gmail.com>
2021-09-18 17:17:10 -0400

    compiling with no JS allowed

--- src/hooks/useArrayField.tsx
+++ src/hooks/useArrayField.tsx
@@ -3,7 +3,7 @@ import useForm from './useForm'
 import get from '../util/get'
 import deepEqual from '../util/deepEqual'
 
-const valueChanged = name => ({ previous, current }) => {
+const valueChanged = (name: string): ShouldUpdateHandler => ({ previous, current }) => {
   if (previous.values === current.values) return false
   const currValue = get(current.values, name)
   const prevValue = get(previous.values, name)
@@ -11,21 +11,33 @@ const valueChanged = name => ({ previous, current }) => {
   return !deepEqual(prevValue, currValue)
 }
 
-const removeIndex = i => arr => {
+const removeIndex = (i: number) => (arr: any[]) => {
   const a = [...arr]
   a.splice(i, 1)
   return a
 }
 
-export default ({ name, Component, props }) => {
+interface Args {
+  name: string
+  Component: any // FIXME
+  props: any // FIXME
+}
+
+interface Result {
+  add: () => void
+  remove: () => void
+  elements: any[] // FIXME
+}
+
+export default ({ name, Component, props }: Args): Result => {
   const shouldUpdate = useCallback(valueChanged(name), [name])
   const { values, setValue } = useForm({ shouldUpdate })
   const arr = get(values, name, [])
 
-  const add = () => setValue(name, [...arr, null])
-  const remove = () => setValue(name, arr.slice(0, -1))
+  const add = (): void => setValue(name, [...arr, null])
+  const remove = (): void => setValue(name, arr.slice(0, -1))
 
-  const elements = arr.map((_, i) =>
+  const elements = arr.map((_: any, i: number) =>
     <Component
       {...props}
       key={i}
